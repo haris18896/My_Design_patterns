82be28876ae78691fc34893eeaa89e6a7a1a6f4a
haris96 <haris@fit-outfit.com>
2022-05-26 17:50:11 +0500

    deleted some files

--- Design Patterns/2- Creational Paterrns/02_04/finished/index.js
+++ Design Patterns/2- Creational Paterrns/02_04/finished/index.js
@@ -1,12 +1,12 @@
-var scout_prototype = require('./scout_prototype');
+var scout_prototype = require('./scout_prototype')
 
-var alex = scout_prototype.clone();
-alex.name = 'Alex Banks';
-alex.addItemToList('slingshot');
+var alex = scout_prototype.clone()
+alex.name = 'Alex Banks' // setting the name
+alex.addItemToList('slingshot')
 
-var eve = scout_prototype.clone();
-eve.name = 'Eve Porcello';
-eve.addItemToList('reading light');
+var eve = scout_prototype.clone()
+eve.name = 'Eve Porcello' // setting the name
+eve.addItemToList('reading light')
 
-console.log( `${alex.name}: ${alex.shoppingList}` );
-console.log( `${eve.name}: ${eve.shoppingList}` );
+console.log(`${alex.name}: ${alex.shoppingList}`)
+console.log(`${eve.name}: ${eve.shoppingList}`)
